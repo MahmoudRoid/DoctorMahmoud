05e84e663cb4551b0ec7042562be431a5651b16f
Mohad <mohamadhadibi@gmail.com>
2016-12-03 23:44:07 -0800

    SendIdeasActivity Created

--- app/src/main/java/ir/unicoce/doctorMahmoud/Activity/ContactUsActivity.java
+++ app/src/main/java/ir/unicoce/doctorMahmoud/Activity/ContactUsActivity.java
@@ -8,19 +8,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import ir.unicoce.doctorMahmoud.Classes.Variables;
 import ir.unicoce.doctorMahmoud.Fragment.MapsFragment;
 import ir.unicoce.doctorMahmoud.Interface.OnFragmentInteractionListener;
 import ir.unicoce.doctorMahmoud.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
-
 
 public class ContactUsActivity extends AppCompatActivity
         implements
@@ -59,6 +56,13 @@ public class ContactUsActivity extends AppCompatActivity
     /*set map fragment to activity*/
     protected void setFragment() {
 
+        /*FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, new MapsFragment());
+        fragmentTransaction.commit();
+*/
+
+        // old
         MapsFragment myFragment = new MapsFragment();
         Bundle bundle = new Bundle();
         bundle.putString("Lat", "41.0760475");
--- app/src/main/java/ir/unicoce/doctorMahmoud/Fragment/MapsFragment.java
+++ app/src/main/java/ir/unicoce/doctorMahmoud/Fragment/MapsFragment.java
@@ -36,14 +36,13 @@ public class MapsFragment extends Fragment {
     private static final String ARG_PARAM3 = "Title";
 
     // TODO: Rename and change types of parameters
-    private String Lat;
-    private String Lng;
+    private String Lat="41.0760475";
+    private String Lng="29.0275024";
     private String MarkerTitle = "";
 
     private OnFragmentInteractionListener mListener;
 
     private GoogleMap mMap;
-    private MapView mapView;
     public LatLng mLatLan =  null;
 
     public MapsFragment() {}
@@ -92,10 +91,6 @@ public class MapsFragment extends Fragment {
         mLatLan = new LatLng(Float.parseFloat(Lat), Float.parseFloat(Lng));
 
         try {
-            mapView = (MapView) layout.findViewById(R.id.map);
-            mapView.onCreate(savedInstanceState);
-
-            mMap = mapView.getMap();
             mMap = ((SupportMapFragment) getActivity()
                     .getSupportFragmentManager()
                     .findFragmentById(R.id.map))
@@ -103,10 +98,9 @@ public class MapsFragment extends Fragment {
             CameraUpdate cam= CameraUpdateFactory.newLatLngZoom(mLatLan,17);
             mMap.animateCamera(cam);
 
-            Marker marker = mMap.addMarker(new MarkerOptions().position(mLatLan)
+            mMap.addMarker(new MarkerOptions().position(mLatLan)
                     .title(MarkerTitle)
                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
-
 
         } // end try
         catch (Exception e) {
